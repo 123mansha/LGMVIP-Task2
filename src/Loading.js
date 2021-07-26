e14334d0b2fcf737f1a5d3dd29d8fd0c22d1fffe
123mansha <manshasrivastava7@gmail.com>
2021-07-26 21:02:19 +0530

    task2 completed

---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      {/* <h1>loading...</h1> */}
+      <i className="fas fa-spinner fa-pulse fa-3x center"  />
+    </div>
+  );
+};
+
+export default Loading;
